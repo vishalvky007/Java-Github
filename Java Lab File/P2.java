b67a69af9b416fe6613ab70c6b6c38986d81ed73
VISHAL KUMAR YADAV <9512278k@gmail.com>
2023-11-30 11:13:17 +0000

    P2,P3 added

---
+++ Java Lab File/P2.java
@@ -0,0 +1,48 @@
+/* Program-2:
+ * Implementing the  concept  of  Wrapper  Classes,  Type  Casting  and
+ * auto-boxing and un-boxing
+ */
+
+class P2 {
+	public static void main(String args[]) {
+
+		int i = 30;
+		float f = 50.0F;
+		char c = 'a';
+		boolean b = true;
+
+		// Autoboxing: Converting primitives into objects
+		Integer intobj = i;
+		Float floatobj = f;
+		Character charobj = c;
+		Boolean boolobj = b;
+
+		// Printing objects
+		System.out.println("\n---Printing object values---");
+		System.out.println("Integer object: " + intobj);
+		System.out.println("Float object: " + floatobj);
+		System.out.println("Character object: " + charobj);
+		System.out.println("Boolean object: " + boolobj);
+
+		// Unboxing: Converting Objects to Primitives
+		int intvalue = intobj.intValue();	//converting Integer to int explicitly  	
+		float floatvalue = floatobj.floatValue();
+		char charvalue = charobj;	//unboxing, now compiler will write a.intValue() internally    
+		boolean boolvalue = boolobj;
+
+		// Printing primitives
+		System.out.println("\n---Printing primitive values---");
+		System.out.println("int value: " + intvalue);
+		System.out.println("float value: " + floatvalue);
+		System.out.println("char value: " + charvalue);
+		System.out.println("boolean value: " + boolvalue);
+
+		// Type casting example
+		double doubleValue = 42.56;
+		int intValue = (int) doubleValue; // Explicit casting (narrowing)
+
+		System.out.println("\nType Casting Example:");
+		System.out.println("Original double: " + doubleValue);
+		System.out.println("Casted int: " + intValue);
+	}
+}
---
+++ Java Lab File/P3.java
@@ -0,0 +1,64 @@
+public class P3 {
+    // Function to demonstrate single-dimensional array
+    void singleDimensionalArrayExample() {
+        System.out.println("Single-Dimensional Array Example:");
+
+        // Declaration and initialization of a single-dimensional array
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        // Accessing and displaying elements of the array
+        System.out.print("Array Elements: ");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // Function to demonstrate multi-dimensional array
+    void multiDimensionalArrayExample() {
+        System.out.println("\nMulti-Dimensional Array Example:");
+
+        // Declaration and initialization of a 2D array
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        // Accessing and displaying elements of the 2D array
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Function to demonstrate ragged array
+    void raggedArrayExample() {
+
+        // Declaration and initialization of a ragged array
+        int[][] raggedMatrix = {
+                {1, 2, 3},
+                {4, 5},
+                {6, 7, 8, 9}
+        };
+
+        // Accessing and displaying elements of the ragged array
+        System.out.println("\nRagged Array Elements:");
+        for (int i = 0; i < raggedMatrix.length; i++) {
+            for (int j = 0; j < raggedMatrix[i].length; j++) {
+                System.out.print(raggedMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        P3 x = new P3();
+        x.singleDimensionalArrayExample();
+        x.multiDimensionalArrayExample();
+        x.raggedArrayExample();
+    }
+}
