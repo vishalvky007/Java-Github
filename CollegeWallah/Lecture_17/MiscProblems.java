ab5d2c54f04d1798ce23cfc3b46af7187c5522dd
vishalvky007 <9512278k@gmail.com>
2024-07-20 11:43:45 +0530

    Lecture 17 completed

--- CollegeWallah/Lecture_17/MiscProblems.java
+++ CollegeWallah/Lecture_17/MiscProblems.java
@@ -34,17 +34,34 @@ public class MiscProblems {
     System.out.println("b: "+b);
   }
 
-  static int[] reverseArray(int[] arr) {
-    int n = arr.length;
-    int[] ans = new int[n];
-    int j = 0;
+  // static int[] reverseArray(int[] arr) {
+  //   int n = arr.length;
+  //   int[] ans = new int[n];
+  //   int j = 0;
 
-    // traverse original array in reverse direction
-    for(int i=n-1; i>=0; i--) {
-      ans[j++] = arr[i];
+  //   // traverse original array in reverse direction
+  //   for(int i=n-1; i>=0; i--) {
+  //     ans[j++] = arr[i];
+  //   }
+
+  //   return ans;
+  // }
+  
+  static void swapInArray(int[] arr, int i, int j) {
+
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  static void reverseArray(int[] arr) {
+    int i=0, j=arr.length-1;
+
+    while(i<j) {
+      swapInArray(arr,i,j);
+      i++;
+      j--;
     }
-
-    return ans;
   }
 
   static void printArray(int[] arr) {
@@ -54,17 +71,82 @@ public class MiscProblems {
     System.out.println();
   }
 
+  static int[] rotate(int[] arr, int k) {
+    int n = arr.length;
+    k = k % n;
+    int[] ans = new int[n];
+    int j = 0;
+
+    for(int i=n-k; i<n; i++) {
+      ans[j++] =  arr[i];
+    }
+
+    for(int i=0; i<n-k; i++) {
+      ans[j++] = arr[i];
+    }
+
+    return ans;
+
+  }
+
+  static void reverseToFrom(int arr[], int i, int j) {
+    while(i<j) {
+      swapInArray(arr, i, j);
+      i++;
+      j--;
+    }
+  }
+
+
+  static void rotateInPlace(int[] arr, int k) {
+    int n = arr.length;
+    k = k % n;
+  
+    reverseToFrom(arr, 0, n-k-1);
+    reverseToFrom(arr, n-k, n-1);
+    reverseToFrom(arr, 0, n-1);
+
+  }
+
+
   public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
 
     int a = 9;
     int b = 3;
-    int[] arr = {1,2,3,4,5,6};
-    printArray(arr);
+    int[] arr = {1,2,3,4,5};
+    // printArray(arr);
 
     // swap(a, b);
     // swapWithoutTemp(a, b);
-    int[] ans = reverseArray(arr);
-    printArray(ans);
+    // int[] ans = reverseArray(arr);
+    // reverseArray(arr);
+    // printArray(arr);  
 
+
+    /* Rotate array main code */
+    /*  
+    System.out.print("Enter k: ");
+    int k = sc.nextInt();
+
+    System.out.print("Original array: ");
+    printArray(arr);
+
+    int[] ans = rotate(arr, k);
+    System.out.print("Array after rotation: ");
+    printArray(ans);
+    */
+    
+    /* Rotate Array In-place */
+    System.out.print("Enter k: ");
+    int k = sc.nextInt();
+
+    System.out.print("Original array: ");
+    printArray(arr);
+
+    rotateInPlace(arr, k);
+    System.out.print("Array after rotation: ");
+    printArray(arr);
   }
 }
---
+++ CollegeWallah/Lecture_17/PresentQuery.java
@@ -0,0 +1,44 @@
+package Lecture_17;
+
+import java.util.Scanner;
+
+public class PresentQuery {
+
+  static int[] makeFrequencyArray(int[] arr) {
+    int[] freq = new int[100000];
+
+    for(int i=0; i<arr.length; i++) {
+      freq[arr[i]]++;
+    }
+
+    return freq;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter size of array: ");
+    int n = sc.nextInt();
+
+    int[] arr = new int[n];
+    System.out.println("Enter " + n + " elements: ");
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = sc.nextInt();
+    }
+
+    int[] freq = makeFrequencyArray(arr);
+
+    System.out.print("Enter number of queires: ");
+    int q = sc.nextInt();
+
+    while(q > 0) {
+      System.out.print("Enter number to be searched: ");
+      int x = sc.nextInt();
+      if(freq[x] > 0) {
+        System.out.println("Yes");
+      } else {
+        System.out.println("No");
+      }
+      q--;
+    }
+  }
+}
